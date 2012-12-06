3d7d6ae51b6132340f1a7ef67978a707a26ef975
wcj343169893@163.com <wcj343169893@163.com@d7bc8717-0a52-b3c1-b193-7d876fda62f6>
2012-12-06 16:10:53 +0000

    数据库修改

---
+++ Ibale/info/data.sql
@@ -0,0 +1,10 @@
+--数据库修改记录
+--article表中增加
+--is_recommend	是否推荐	integer
+--pic_url		图片地址	character
+
+--brand 品牌表
+--is_recommend	是否推荐	integer
+
+--product 产品表
+--recommended_rating -- 推荐等级：0：不推荐，1：推荐到列表页，2：推荐到首页
